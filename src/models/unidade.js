256148a67583de4c93f5786bd76e666dad464263
Andre Luiz Antunes <andrelfantunes@gmail.com>
2019-05-09 14:52:36 -0300

    First commit

---
+++ src/models/unidade.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const Unidade = mongoose.model('Unidade', {
+    nome: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    estado: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    latitude: {
+        type: Number,
+        required: true,
+    },    
+    longitude: {
+        type: Number,
+        required: true,
+    },    
+    planos_atendidos: {
+        type: [String], 
+        required: true,
+        validate: [(value) => value.length > 0, 'Field "planos_atendidos" cannot be empty']
+    },
+    img: {
+        type: String,
+        required: true
+    }   
+    
+})
+
+module.exports = Unidade
