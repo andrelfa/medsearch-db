30d36e855f68abcf7a6c0972fff405337ff7c008
Andre Luiz Antunes <andrelfantunes@gmail.com>
2019-07-09 19:42:23 -0300

    Minor adjustments

--- src/index.js
+++ src/index.js
@@ -33,48 +33,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users);
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id;
-    User.findById(_id).then((user) => {
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    }).catch((e) => {
-        console.log(e);
-        res.status(500).send();
-    })
-})
-
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
 app.post('/unidade', (req, res) => {
     const unidade = new Unidade(req.body)
 
